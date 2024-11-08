8565abdab24560c34723aed487d620df33ef5b35
Vamsi Dath .M <vamsiduth.meka@gmail.com>
2024-11-08 05:24:53 -0600

    practised bash/terminal commands and created UI/UX folder for topic specific content

---
+++ Other Files/new_script.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+echo "Script execution started"
+mkdir automated_dir
+cd automated_dir && touch newscriptfile.txt
+cat >> newscriptfile.txt
+cat newscriptfile.txt
+echo "Script execution finished"
+
